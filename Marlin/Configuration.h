612f0b684ffbeb82ff8d55252dbbcd1b122dde01
Willyam Luccas <willyamluccas@gmail.com>
2018-06-20 21:37:30 -0300

    Pid do hotend arrumado

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -355,9 +355,9 @@
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
   // Ultimaker
-  #define  DEFAULT_Kp 22.2
-  #define  DEFAULT_Ki 1.08
-  #define  DEFAULT_Kd 114
+  #define  DEFAULT_Kp 36.43
+  #define  DEFAULT_Ki 03.67
+  #define  DEFAULT_Kd 90.28
 
   // MakerGear
   //#define  DEFAULT_Kp 7.0
